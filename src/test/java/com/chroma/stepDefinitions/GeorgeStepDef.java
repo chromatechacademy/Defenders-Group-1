2ccfc7e3e1b3a815f5efa53e71fc1c4da8943676
George B <GeorgeB@Georges-MBP.fios-router.home>
2023-01-31 23:12:04 -0500

    Renamed classes and added CommonMethids

--- src/test/java/com/chroma/stepDefinitions/GeorgeStepDef.java
+++ src/test/java/com/chroma/stepDefinitions/GeorgeStepDef.java
@@ -2,10 +2,11 @@ package com.chroma.stepDefinitions;
 
 import com.chroma.appsCommon.PageInitializer;
 
-public class SampleStepDefinitions extends PageInitializer{
+public class GeorgeStepDef extends PageInitializer {
 
     // Add step definitions in this class
     // NOTE: Every step definitions class Extends PageInitializer
     // PageInitializer extends WebDriverUtils
-    
+
+
 }
