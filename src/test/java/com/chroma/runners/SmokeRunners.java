ae165cf8ac99e93931b1e416787b00966c88caa0
George B <GeorgeB@Georges-MBP.fios-router.home>
2023-02-02 19:08:36 -0500

    New4

--- src/test/java/com/chroma/runners/SmokeRunners.java
+++ src/test/java/com/chroma/runners/SmokeRunners.java
@@ -11,12 +11,12 @@ import cucumber.api.junit.Cucumber;
 		"pretty", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"  }, 
 		features = "src/test/java/com/chroma/features", 
 		glue = "com.chroma.stepDefinitions", 
-		tags = "@CTSMSProgression", 
+		tags = "@Smoke", 
 		dryRun =  false, 
 		monochrome = true, 
 		strict = true
 ) 
 
-public class ProgressionRunners {
+public class SmokeRunners {
     
 }
