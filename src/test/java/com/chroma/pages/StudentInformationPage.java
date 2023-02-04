383c2c678bee9f45f87d6ae0cf865fc031510db6
Alexandra <alexkosheeva@gmail.com>
2023-02-03 20:37:30 -0500

    update

--- src/test/java/com/chroma/pages/StudentInformationPage.java
+++ src/test/java/com/chroma/pages/StudentInformationPage.java
@@ -3,7 +3,6 @@ package com.chroma.pages;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-
 import com.chroma.web.WebDriverUtils;
 
 public class StudentInformationPage {
@@ -38,6 +37,5 @@ public class StudentInformationPage {
 
     public StudentInformationPage() {
         PageFactory.initElements(WebDriverUtils.driver, this);
-    }
-  
+    } 
 }
