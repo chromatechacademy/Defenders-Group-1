fc87d5f4dc1759cbee74adab1956eca5a4cf3a27
George B <GeorgeB@Georges-MBP.fios-router.home>
2023-02-04 20:51:44 -0500

    All the User Stories PASS and new set of data chenged for DF-12, DF-13 and DF-45 Updated

--- src/test/java/com/chroma/runners/CTSMSProgressionRunners.java
+++ src/test/java/com/chroma/runners/CTSMSProgressionRunners.java
@@ -10,7 +10,7 @@ import cucumber.api.junit.Cucumber;
 		"pretty", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}, 
 		features = "src/test/java/com/chroma/features", 
 		glue = "com.chroma.stepDefinitions", 
-		tags = "@DF-4", 
+		tags = "@CTSMSProgression", 
 		dryRun = false, 
 		monochrome = true, 
 		strict = true
