67e35bdf935e691b7b3f6c75199524ef1278088d
Alexandra <alexkosheeva@gmail.com>
2023-02-05 00:06:58 -0500

    update

--- src/test/java/com/chroma/stepDefinitions/CTSMSStudentAdmissionStepDef.java
+++ src/test/java/com/chroma/stepDefinitions/CTSMSStudentAdmissionStepDef.java
@@ -3,6 +3,7 @@ package com.chroma.stepDefinitions;
 import com.chroma.appsCommon.PageInitializer;
 
 public class CTSMSStudentAdmissionStepDef extends PageInitializer{
-   /* StudentAdmissionStepDef is used for methods */   
+   
+    /* StudentAdmissionStepDef is used for methods */   
 }
 
